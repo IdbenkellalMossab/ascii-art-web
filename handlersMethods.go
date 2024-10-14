0506d47268a2305a17b5098042fd765c6bc0e29b
midbenke <idmossab@gmail.com>
2024-10-14 16:42:20 +0100

    separe all methode in files

---
+++ handlersMethods.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		// Display the homepage
+		renderTemplate(w, "Home Page", &res)
+		res = result{
+			Res:  "", // Clear previous values
+			Res1: "",
+			Err:  "",
+		}
+	} else {
+		// Return an error if the path is incorrect
+		errorHandler(w, http.StatusNotFound)
+	}
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/ascii-art" {
+		// Handle POST requests for /ascii-art
+		r.ParseForm()
+		text := r.FormValue("text")
+		banner := r.FormValue("banner")
+		if text == "" || banner == "" { // Set error message if any field is empty
+			setError(w, r, "Text or Banner cannot be empty")
+			return
+		}
+		if len(text) > 700 { // Check if text exceeds 700 characters
+			setError(w, r, "Please enter less than 700 characters.")
+			return
+		}
+		// Generate ascii-art
+		artResult := artHandler(text, banner)
+
+		// Check if special characters are present
+		if artResult == "Special charactere is not allowed." {
+			// Set error message for non-printable characters
+			setError(w, r, "Please enter printable ASCII characters only.")
+			return
+		} else {
+			// Process form values and generate ASCII art if valid
+			res = result{
+				Res:  banner,
+				Res1: "\n" + artResult,
+				Err:  "", // Clear error
+			}
+		}
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	} else {
+		// 404 Error for incorrect path
+		errorHandler(w, http.StatusNotFound)
+	}
+}
--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+)
+
+type result struct {
+	Res  string
+	Res1 string
+	Err  string
+}
+
+var (
+	templates = template.Must(template.ParseGlob("Templates/*.html"))
+	res       result
 )
 
 func main() {
@@ -10,4 +22,29 @@ func main() {
 
 	fmt.Println("Server is running at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// handleGet processes GET requests and renders the home page.
+		handleGet(w, r)
+	case http.MethodPost:
+		// handlePost processes POST requests for the ASCII Art generation.
+		handlePost(w, r)
+	default:
+		// 405 Error for unsupported methods
+		errorHandler(w, http.StatusMethodNotAllowed)
+	}
+}
+
+func renderTemplate(w http.ResponseWriter, title string, result *result) {
+	err := templates.ExecuteTemplate(w, "layout.html", map[string]interface{}{
+		"Title":  title,
+		"Result": result, // Pass the result to the template
+	})
+	// Server error
+	if err != nil {
+		errorHandler(w, http.StatusInternalServerError)
+	}
 }
